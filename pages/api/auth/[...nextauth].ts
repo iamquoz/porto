fd1199067c6a0a6fb755acea423a2528d978b1c7
iamquoz <fylhtq30rjrjkjd@gmail.com>
2022-03-06 18:36:41 +0400

    add yandex provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import VkProvider from "next-auth/providers/vk";
 import GitHubProvider from "next-auth/providers/github";
+import YandexProvider from "next-auth/providers/yandex"
+
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import bcrypt from 'bcrypt'
 
@@ -21,6 +23,10 @@ export default NextAuth({
 		GitHubProvider({
 			clientId: process.env.GITHUB_CLIENT_ID,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET
+		}),
+		YandexProvider({
+			clientId: process.env.YANDEX_CLIENT_ID,
+			clientSecret: process.env.YANDEX_CLIENT_SECRET
 		})
 	],
 	secret: process.env.secret
